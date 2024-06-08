fbca5eb4f9346fb6fd1a5c96e1e7848c20e6a41c
Kylee Sawyer <k.sawyer@ufl.edu>
2024-06-08 16:05:09 -0400

    added to queries 5,6,7,11

--- queries/05-join-artist-album.sql
+++ queries/05-join-artist-album.sql
@@ -1,3 +1,9 @@
 -- Join artists on albums, pulling artists.name as artist_name
 -- albums.name as album_name and albums.release_year
 -- sort by artist name and album name ascending
+SELECT
+    artists.name AS "artist_name",
+    albums.name AS "album_name",
+    albums.release_year AS "album_release_year"
+FROM albums
+LEFT JOIN 
